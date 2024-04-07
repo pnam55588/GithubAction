73fc09e5771bda7b052b355a0f43e9235f1b64c6
Phạm Hà Nam <pnam55588@gmail.com>
2024-04-07 12:59:26 +0700

    add simple test

---
+++ src/counter/Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Counter = (props) => {
+    const [count, setCount] = useState(props.initialCount || 0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div>
+            <h1>Counter</h1> <h1 data-testid="count">{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
---
+++ src/counter/Counter.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Counter from './Counter';
+
+
+describe(Counter,()=>{
+    it('increments the counter when the increment button is clicked', () => {
+        const { getByTestId, getByRole} = render(<Counter initialCount={0}/>);
+        const incrementButton = getByRole('button', { name: 'Increment' });
+        fireEvent.click(incrementButton);
+        const counterValue = Number(getByTestId('count').textContent);
+        expect(counterValue).toEqual(1);
+    });
+    it('increments the counter when the increment button is clicked', () => {
+        const { getByTestId, getByRole} = render(<Counter initialCount={0}/>);
+        const decrementButton = getByRole('button', { name: 'Decrement' });
+        fireEvent.click(decrementButton);
+        const counterValue = Number(getByTestId('count').textContent);
+        expect(counterValue).toEqual(-1);
+    });
+} );
